da6792bc8d4335298e5f8b3165db80c5c9a2d4d7
rafi-3 <nws04_01089114@pb.edu.bn>
2019-09-11 14:55:29 +0800

    update
    Signed-off-by: rafi-3 <nws04_01089114@pb.edu.bn>

--- Programming_1/Practice4/q3.java
+++ Programming_1/Practice4/q3.java
@@ -4,33 +4,37 @@ import java.util.Scanner;
 
 public class q3 {
 	public static void main(String[] args) {
+	    
+	    Scanner scanner = new Scanner (System.in);
+	    
+	    int [] distinctNumber = new int [10];
+	    int count = 0;
+	    
+	    System.out.println("Enter ten numbers: ");
+	    int number = scanner.nextInt();
+	    
+	    for (int i = 0; i < 10; i ++) {
+	      number = scanner.nextInt();
+	      boolean distinct = true;
+	      
+	      for (int j = 0; j < count ; j++)
+	        if (number == distinctNumber [j]) {
+	          distinct = false;
+	          break;
+	        }
+	      if (distinct) {
+	        distinctNumber[count++] = number;
+	    
+	      }
 
-        Scanner input = new Scanner(System.in);
-        int[] numbers = new int[10];
-        int index = 0;
-        System.out.print("Enter ten numbers: ");
-
-
-        for (int i = 0; i < numbers.length; i++) {
-            int num = input.nextInt();
-
-            if (isNew(numbers, num)) {
-                numbers[index++] = num;
-            }
-        }
-        System.out.println("The number of distinct number is " + index);
-        System.out.print("The distinct numbers are: ");
-        for (int i = 0; i < index; i++) {
-            System.out.print(numbers[i] + " ");
-        }
-    }
-
-    public static boolean isNew(int[] numbers, int n) {
-
-        for (int i : numbers){
-            if (n == i) return false;
-        }
-        return true;
-    }
-
+	    }
+	    
+	    System.out.println(" ");
+	    System.out.println("The number of distinct number is: " + (count -1));
+	    System.out.println("The Distinct Numbers are: ");
+	    
+	    for (int i=0; i<count; i++) {
+	      System.out.print(distinctNumber[i] + " ");
+	    }
+	}
 }
